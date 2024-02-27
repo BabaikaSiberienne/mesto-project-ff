573268eb75514da1aac3827dda9db658b7a2f8f2
Niya <awesome.flure@yandex.ru>
2024-02-27 11:47:13 +0300

    логика выведения карточки на страницу изменена, добавлен alt

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,25 @@ const placesContainer = document.querySelector('.places__list'); //берем к
 const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонируем элемент
 // @todo: Функция создания карточки
 
-initialCards.forEach((card) => {
-        const cardTemplate = document.querySelector('#card-template').content //получаем содержимое шаблона
-        const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонируем элемент
-        cardElement.querySelector('.card__image').src = card.link; //присваиваем картинке путь из массива js файла
-        cardElement.querySelector('.card__title').textContent = card.name; //присваиваем тайтлу текст из массива js файла
-        const delButton = cardElement.querySelector('.card__delete-button'); //берем кнопку удаления
-        delButton.addEventListener('click', delCard);
-    placesContainer.append(cardElement); //добавляем в коробку с местами места
-});
+function createCard (card, handDel) {
+    const cardTemplate = document.querySelector('#card-template').content //получаем содержимое шаблона
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонируем элемент
+    cardElement.querySelector('.card__image').src = card.link; //присваиваем картинке путь из массива js файла
+    cardElement.querySelector('.card__title').textContent = card.name; //присваиваем тайтлу текст из массива js файла
+    cardElement.querySelector('.card__image').alt = card.alt; //присваиваем картинке альтернативное описание из массива js файла
+    const delButton = cardElement.querySelector('.card__delete-button'); //берем кнопку удаления
+    delButton.addEventListener('click', delCard);
+    return cardElement
 
+}
 // @todo: Функция удаления карточки
 function delCard (event) {
     const cardElementClosest = event.target.closest('.card')
     cardElementClosest.remove()
 }
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+    placesContainer.append(createCard(card))
+});
+
